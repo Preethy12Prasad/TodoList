4a480be22296c3698f82a23bf705e145f06147a1
Preethy12Prasad <149069553+Preethy12Prasad@users.noreply.github.com>
2024-06-24 11:10:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,181 @@
+//import College from './component/Student';
+import logo from './logo.svg';
+import React from 'react';
+import ToDoList from './component/ToDoList';
+
+// import './App.css';
+// import Todo from './component/Todo';
+// import TodoForm from './component/TodoForm';
+// import TodoWrapper from './component/TodoWrapper';
+// import EditTodoForm from './component/EditTodoForm';
+// import'./css/styles.css';
+// // import 'bootstrap/scss/bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Card from 'react-bootstrap/Card';
+ 
+//import Display from './component/Previous';
+//import Demo from './component/Greater';
+//import Ref from './component/Reference';
+//import Count from './component/Count';
+//import University from './component/Context';
+//import Register from './component/Registration';
+//import Login from './component/Login';
+//import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+//import Post from './component/Application';
+//import React, { useState } from 'react';
+//import { BrowserRouter as Router,Routes, Route, Switch, Link } from 'react-router-dom';
+//import { Navbar, Nav, Container } from 'react-bootstrap';
+
+//import Main from './component/Applicationtable';
+// import LoginPage from './component/Login';
+// import RegistrationPage from './component/RegLogin';
+// import WelcomePage from './component/LoginWelcome';
+// import { PopoverHeader } from 'react-bootstrap';
+// import Post from './component/Application';
+//import Home from './component/work/Home';
+//import TodoList from './component/work/Todolist';
+//import React from 'react';
+// import Header from './component/Header';
+// import Form from './component/Form';
+// import TODOHero from './component/TODOHero';
+// import Home from './component/work/Home';
+// import Item from './component/Item';
+// import TODOList from './component/TODOList';
+//import AddTodo from './component/work/AddTodo';
+//import MyForm from './component/Form';
+//import Welcome from './component/Welcome';
+//import Welcome1 from './component/Welcome1';
+//import MyForm from './component/Form';
+//import MyForms from './component/FormButton';
+//import Garage1 from './component/Garage1';
+//import List from './component/Student';
+//import Garage from './component/Garage';
+//import Sum from './component/Sum';
+//import Screen from './component/Screen';
+//import Counter from './component/Counter';
+//import Greet from './component/greeting';
+/*import Car from './component/Car'; 
+
+
+
+function App() {
+  return (
+    <Car colour="red"/>
+  
+  );
+  
+}*/
+
+/*function App(){
+  return(
+ // < Sum  n1={3} n2={4}/>
+   //<Counter/>
+   <Greet/>
+  );
+}*/
+/*function App(){
+  return(
+   // <Screen isScreen={true}/>
+   //<Garage bikes={["r15","duke","kinetic"]}/>
+<Garage1/>
+  )
+}*/
+//function App(){
+ // return(
+   // <List/>
+   //<MyForm/>
+   //<MyForms/>
+   //<Welcome/>
+  
+
+//{/* <Router>
+      //<Routes>
+        {/*<Route path="/" element={<Main/>}/>*/}
+        //<Route path="/" element={<Main/>}/>
+      // {/* <Route path="/welcome" element={<Welcome/>}/>*/}
+      //</Routes>
+    //</Router>
+  //)
+//}
+// }
+   /*function App(){
+      
+      return(
+//       <Count/>
+//<University/> 
+       //<LoginPage/>
+      //<Demo/>
+     //  <Post/>
+     <MyForm/>
+     )
+    }  
+    
+
+ export default App;*/
+ 
+ /*function App(){
+      
+  return(
+<Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  
+ )
+}  
+
+
+export default App;*/
+
+//  import React from "react";
+//  import Home from "./Home";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       < Form/>
+//     </div>
+//   );
+// }
+
+
+
+// export default App;
+
+/*function App(){
+      
+  return(
+<Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/Header" element={<Header />} />
+          <Route path="/Item" element={<Item />} />
+          <Route path="/TODOHero" element={<TODOHero />} />
+          <Route path="/TODOList" element={<TODOList/>} />
+        </Routes>
+      </div>
+    </Router>
+  
+ )
+}  
+
+
+export default App;*/
+function App(){
+  return(
+<div>
+  <ToDoList/>
+
+</div>
+
+  )
+}
+
+export default App;
+  
